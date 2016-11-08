9c4a095c030ce9bf0c52727f058a2a1211fae1ad
TIYZAP <ironyardzap@gmail.com>
2016-11-08 09:26:26 -0500

    Combined both tables

--- main.js
+++ main.js
@@ -1,53 +1,103 @@
 movies = []
 
 movies.push({
-title: "Enter The Dragon",
-director: "Robert Clouse",
-genre: "Martial Arts",
-year: "1973",
-actors: ["Bruce Lee","John Saxon","Jim Kelly","Ahna Capri", "Shih Kien"],
-stars: "7.7",
-metascore: "N/A"
+  title: "Enter The Dragon",
+  director: "Robert Clouse",
+  genre: "Martial Arts",
+  year: "1973",
+  actors: ["Bruce Lee","John Saxon","Jim Kelly","Ahna Capri", "Shih Kien"],
+  stars: "7.7",
+  metascore: "N/A"
 })
 
 movies.push({
-title: "The Empire Strikes Back",
-director: "Irvin Kershner",
-genre: "Sci Fi",
-year: "1980",
-actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams","Anthony Daniels", "Daivd Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"],
-stars: "8.8",
-metascore: "80"
+  title: "The Empire Strikes Back",
+  director: "Irvin Kershner",
+  genre: "Sci Fi",
+  year: "1980",
+  actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams","Anthony Daniels", "Daivd Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"],
+  stars: "8.8",
+  metascore: "80"
 })
 
 movies.push({
-title: "The Karate Kid pt.1",
-director: "John G. Avildsen",
-genre: "Martial Arts",
-year: "1984",
-actors: ["Ralph Macchio", "Noriyuki Pat Morita", "Elisabeth Shue"],
-stars: "7.2",
-metascore: "N/A"
+  title: "The Karate Kid pt.1",
+  director: "John G. Avildsen",
+  genre: "Martial Arts",
+  year: "1984",
+  actors: ["Ralph Macchio", "Noriyuki Pat Morita", "Elisabeth Shue"],
+  stars: "7.2",
+  metascore: "N/A"
 })
 
 movies.push({
-title: "Scarface",
-director: "Brian De Palma",
-genre: "Crime Drama",
-year: "1983",
-actors: ["Al Pacino","Steven Bauer", "Michelle Pfeiffer", "Mary Mastrantonio", "Paul Shenar", "Robert Loggia", "F.Murry Abraham", "Harris Yulin"],
-stars: "8.3",
-metascore: "65"
+  title: "Scarface",
+  director: "Brian De Palma",
+  genre: "Crime Drama",
+  year: "1983",
+  actors: ["Al Pacino","Steven Bauer", "Michelle Pfeiffer", "Mary Mastrantonio", "Paul Shenar", "Robert Loggia", "F.Murry Abraham", "Harris Yulin"],
+  stars: "8.3",
+  metascore: "65"
 })
 
 movies.push({
-title: "The Big Lebowski",
-director: "Joel Coen",
-genre: "Comedy",
-year: "1998",
-actors: ["Jeff Bridges", "John Goodman","Julianne Moore", "Steve Buscemi","David Huddleston","John Turturro"],
-stars: "8.2",
-metascore: "69"
+  title: "The Big Lebowski",
+  director: "Joel Coen",
+  genre: "Comedy",
+  year: "1998",
+  actors: ["Jeff Bridges", "John Goodman","Julianne Moore", "Steve Buscemi","David Huddleston","John Turturro"],
+  stars: "8.2",
+  metascore: "69"
+})
+
+movies.push({
+  title: "Gymkata - The Deadly Game",
+  director: "Robert Clouse",
+  genre: "Martial Arts",
+  year: "1985",
+  actors: ["Olympic Gold Medalist Kurt Thomas!!!"],
+  stars: "4.2",
+  metascore: "N/A"
+})
+
+movies.push({
+  title: "Battlefield Earth",
+  director: "Roger Christian",
+  genre: "Sci Fi",
+  year: "2000",
+  actors: ["John Travolta", "Barry Pepper", "Forest Whitaker", "Kim Coates", "Sabine Karsenti", "Richard Tyson"],
+  stars: "2.4",
+  metascore: "9"
+})
+
+movies.push({
+  title: "Batman & Robin",
+  director: "Joel Schumacher",
+  genre: "Action",
+  year: "1997",
+  actors: ["George Clooney", "Chris O'Donnell", "Alicia Silverstone", "Uma Thurman", "Arnold Schwarzenegger"],
+  stars: "3.7",
+  metascore: "28"
+})
+
+movies.push({
+  title: "Street Fighter: The Legend of Chun Li",
+  director: "Andrzej Bartkowiak",
+  genre: "Action",
+  year: "2009",
+  actors: ["Kristin Kreuk", "Chris Klien", "Neal McDonough", "Robin Shou", "Moon Bloodgood", "Josie Ho", "Taboo", "Michael Clark Duncan "],
+  stars: "3.7",
+  metascore: "17"
+})
+
+movies.push({
+  title: "Anything with Tyler Perry",
+  director: "Some hack",
+  genre: "Terrible",
+  year: "19XX",
+  actors: ["He's garbage!"],
+  stars: "0.0",
+  metascore: "ZERO"
 })
 
 // Sort by title!
@@ -58,82 +108,12 @@ title = movies.sort(function(movies, other){
 
 // Sort by Star Rating!
 
-  star_rating = movies.sort(function(movies, other){
-    return parseFloat(other.stars) - parseFloat(movies.stars)
-  })
+star_rating = movies.sort(function(movies, other){
+  return parseFloat(other.stars) - parseFloat(movies.stars)
+})
 
 movies.forEach(function(mov) {
   document.getElementById('movie_list').innerHTML += "<td>" + mov.title + "</td>" + "<td>" + mov.director +"</td>" + "<td>"+ mov.genre + "</td>" +  "<td>" + mov.year + "</td>" + "<td>" + mov.actors + "</td>"
-  + "<td>" + mov.stars + "</td>" + "<td>" + mov.metascore + "</td>"
-})
-
-
-bad_movies = []
-
-bad_movies.push({
-title: "Gymkata - The Deadly Game",
-director: "Robert Clouse",
-genre: "Martial Arts",
-year: "1985",
-actors: ["Olympic Gold Medalist Kurt Thomas!!!"],
-stars: "4.2",
-metascore: "N/A"
-})
-
-bad_movies.push({
-title: "Battlefield Earth",
-director: "Roger Christian",
-genre: "Sci Fi",
-year: "2000",
-actors: ["John Travolta", "Barry Pepper", "Forest Whitaker", "Kim Coates", "Sabine Karsenti", "Richard Tyson"],
-stars: "2.4",
-metascore: "9"
-})
-
-bad_movies.push({
-title: "Batman & Robin",
-director: "Joel Schumacher",
-genre: "Action",
-year: "1997",
-actors: ["George Clooney", "Chris O'Donnell", "Alicia Silverstone", "Uma Thurman", "Arnold Schwarzenegger"],
-stars: "3.7",
-metascore: "28"
-})
-
-bad_movies.push({
-title: "Street Fighter: The Legend of Chun Li",
-director: "Andrzej Bartkowiak",
-genre: "Action",
-year: "2009",
-actors: ["Kristin Kreuk", "Chris Klien", "Neal McDonough", "Robin Shou", "Moon Bloodgood", "Josie Ho", "Taboo", "Michael Clark Duncan "],
-stars: "3.7",
-metascore: "17"
-})
-
-bad_movies.push({
-title: "Anything with Tyler Perry",
-director: "Some hack",
-genre: "Terrible",
-year: "19XX",
-actors: ["He's garbage!"],
-stars: "0.0",
-metascore: "ZERO"
-})
-
-// Sort by title!
-
-title = bad_movies.sort(function(movies, other){
-  return bad_movies.title > other.title
-});
-
-// Sort by Star Rating!
-
-  star_rating = bad_movies.sort(function(movies, other){
-    return parseFloat(other.stars) - parseFloat(movies.stars)
-  })
-
-bad_movies.forEach(function(mov) {
-  document.getElementById('bad_list').innerHTML += "<td>" + mov.title + "</td>" + "<td>" + mov.director +"</td>" + "<td>"+ mov.genre + "</td>" +  "<td>" + mov.year + "</td>" + "<td>" + mov.actors + "</td>"
   + "<td>" + mov.stars + "</td>" + "<td>" + mov.metascore + "</td>"
 })
 
