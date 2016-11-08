8beebe3773b54a3882274a84b46829f47ac75d23
TIYZAP <ironyardzap@gmail.com>
2016-11-07 21:43:42 -0500

    Rated by stars

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ title: "Enter The Dragon",
 director: "Robert Clouse",
 genre: "Martial Arts",
 year: "1973",
-actors: ["Bruce Lee","John Saxon","Jim Kelly","Ahna Capri", "Shih Kien"]
+actors: ["Bruce Lee","John Saxon","Jim Kelly","Ahna Capri", "Shih Kien"],
+stars: "7.7",
+metascore: "N/A"
 })
 
 movies.push({
@@ -13,7 +15,9 @@ title: "The Empire Strikes Back",
 director: "Irvin Kershner",
 genre: "Sci Fi",
 year: "1980",
-actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams","Anthony Daniels", "Daivd Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"]
+actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams","Anthony Daniels", "Daivd Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"],
+stars: "8.8",
+metascore: "80"
 })
 
 movies.push({
@@ -21,7 +25,9 @@ title: "The Karate Kid pt.1",
 director: "John G. Avildsen",
 genre: "Martial Arts",
 year: "1984",
-actors: ["Ralph Macchio", "Noriyuki Pat Morita", "Elisabeth Shue"]
+actors: ["Ralph Macchio", "Noriyuki Pat Morita", "Elisabeth Shue"],
+stars: "7.2",
+metascore: "N/A"
 })
 
 movies.push({
@@ -29,7 +35,9 @@ title: "Scarface",
 director: "Brian De Palma",
 genre: "Crime Drama",
 year: "1983",
-actors: ["Al Pacino","Steven Bauer", "Michelle Pfeiffer", "Mary Mastrantonio", "Paul Shenar", "Robert Loggia", "F.Murry Abraham", "Harris Yulin"]
+actors: ["Al Pacino","Steven Bauer", "Michelle Pfeiffer", "Mary Mastrantonio", "Paul Shenar", "Robert Loggia", "F.Murry Abraham", "Harris Yulin"],
+stars: "8.3",
+metascore: "65"
 })
 
 movies.push({
@@ -37,15 +45,26 @@ title: "The Big Lebowski",
 director: "Joel Coen",
 genre: "Comedy",
 year: "1998",
-actors: ["Jeff Bridges", "John Goodman","Julianne Moore", "Steve Buscemi","David Huddleston","John Turturro"]
+actors: ["Jeff Bridges", "John Goodman","Julianne Moore", "Steve Buscemi","David Huddleston","John Turturro"],
+stars: "8.2",
+metascore: "69"
 })
+
+// Sort by title!
 
 title = movies.sort(function(movies, other){
   return movies.title > other.title
 });
 
+// Sort by Star Rating!
+
+  star_rating = movies.sort(function(movies, other){
+    return parseFloat(other.stars) - parseFloat(movies.stars)
+  })
+
 movies.forEach(function(mov) {
   document.getElementById('movie_list').innerHTML += "<td>" + mov.title + "</td>" + "<td>" + mov.director +"</td>" + "<td>"+ mov.genre + "</td>" +  "<td>" + mov.year + "</td>" + "<td>" + mov.actors + "</td>"
+  + "<td>" + mov.stars + "</td>" + "<td>" + mov.metascore + "</td>"
 })
 
 
